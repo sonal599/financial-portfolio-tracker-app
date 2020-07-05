27b5ccd0f467ec44b6ad0d20530a48ebd3269dfd
sonal599 <59963479+sonal599@users.noreply.github.com>
2020-07-06 00:49:42 +0530

    Update ApiCall.js

--- src/Components/ApiCall.js
+++ src/Components/ApiCall.js
@@ -16,7 +16,7 @@ class ApiCall extends Component {
         let sym = symbol;
         let buy =buyprice;
         let sh = share;
-        const apikey="8ITBD22C4L2XEYPM";
+        const apikey="NABQMJ3AXY78UIP8";
         const api_url=`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${sym}&apikey=${apikey}`;
             axios.get(api_url)
             .then((response)=>{
